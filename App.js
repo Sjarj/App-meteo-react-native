a54c83608018d58c00df237f5cb775828b3b6897
Sjarj <sebastien.jarjanette@icloud.com>
2019-11-28 23:13:35 +0100

    Add mapView

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+const DEFAUT_COORD = {
+  lat: 48.859268,
+  lng: 2.34706
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView
+        style={{ flex: 1 }}
+        region={{
+          latitude: DEFAUT_COORD.lat,
+          longitude: DEFAUT_COORD.lng,
+          latitudeDelta: 0.2,
+          longitudeDelta: 0.1
+        }}
+        scrollEnabled={false}
+        liteMode={true}
+      ></MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex: 1
+  }
 });
