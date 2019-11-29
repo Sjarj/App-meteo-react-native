886524a6ea690f1b74659f131536a6a8145e3a83
Sjarj <sebastien.jarjanette@icloud.com>
2019-11-29 10:35:01 +0100

    Add SearchBar

--- App.js
+++ App.js
@@ -1,32 +1,8 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import MapView from 'react-native-maps';
+import React, { Component } from 'react';
+import SearchScreen from './screens/search-screen';
 
-const DEFAUT_COORD = {
-  lat: 48.859268,
-  lng: 2.34706
-};
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <MapView
-        style={{ flex: 1 }}
-        region={{
-          latitude: DEFAUT_COORD.lat,
-          longitude: DEFAUT_COORD.lng,
-          latitudeDelta: 0.2,
-          longitudeDelta: 0.1
-        }}
-        scrollEnabled={false}
-        liteMode={true}
-      ></MapView>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
+export default class App extends Component {
+  render() {
+    return <SearchScreen />;
   }
-});
+}
---
+++ screens/search-screen.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
+import { SearchBar } from 'react-native-elements';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from 'react-native-responsive-screen';
+
+const DEFAUT_COORD = {
+  lat: 48.859268,
+  lng: 2.34706
+};
+
+export default class SearchScreen extends React.Component {
+  state = { search: '' };
+
+  updateSearch = search => {
+    this.setState({ search });
+  };
+
+  submitSearch = () => {
+    console.log(this.state.search);
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={{ flex: 1 }}
+          region={{
+            latitude: DEFAUT_COORD.lat,
+            longitude: DEFAUT_COORD.lng,
+            latitudeDelta: 0.2,
+            longitudeDelta: 0.1
+          }}
+          scrollEnabled={false}
+          liteMode={true}
+        />
+        <SearchBar
+          lightTheme
+          onChangeText={this.updateSearch}
+          value={this.state.search}
+          onSubmitEditing={this.submitSearch}
+          placeholder='type your city...'
+          containerStyle={{
+            position: 'absolute',
+            bottom: hp('50%'),
+            left: wp('5%'),
+            width: wp('90%')
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
