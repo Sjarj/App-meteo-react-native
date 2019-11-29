6b78eae9a68b26ebb800f29620da348c1f14b292
Sjarj <sebastien.jarjanette@icloud.com>
2019-11-29 13:04:12 +0100

    Add action

---
+++ constant.js
@@ -0,0 +1,1 @@
+export default API_KEY = '253883716a369a7fae362534a1ef8571';
--- reducers/weather.js
+++ reducers/weather.js
@@ -1,5 +1,13 @@
+import { SET_CURRENT_WEATHER } from '../action-types';
+
 const initialState = {};
 
 export default (state = initialState, { type, payload }) => {
-  return { data: 'blabla' };
+  switch (type) {
+    case SET_CURRENT_WEATHER:
+      return { ...state, ...payload };
+
+    default:
+      return state;
+  }
 };
