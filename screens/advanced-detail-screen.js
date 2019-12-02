3817f8bd755f6e136e65f1158853eb52c7a761e2
Sjarj <sebastien.jarjanette@icloud.com>
2019-12-02 17:06:21 +0100

    Display chart

--- screens/advanced-detail-screen.js
+++ screens/advanced-detail-screen.js
@@ -1,22 +1,83 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import { getForecastWeatherByCity } from '../actions';
+import { LineChart } from 'react-native-chart-kit';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from 'react-native-responsive-screen';
 
 export class AdvancedDetailScreen extends Component {
   componentDidMount() {
     const city = this.props.navigation.getParam('city');
     this.props.getForecastWeatherByCity(city);
-    console.log(this.props);
   }
 
+  renderChart = data => {
+    return (
+      <LineChart
+        data={{
+          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+          datasets: [
+            {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100
+              ]
+            }
+          ]
+        }}
+        width={wp('90%')} // from react-native
+        height={hp('30%')}
+        yAxisLabel={'$'}
+        yAxisSuffix={'k'}
+        chartConfig={{
+          backgroundColor: '#e26a00',
+          backgroundGradientFrom: '#fb8c00',
+          backgroundGradientTo: '#ffa726',
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16
+          },
+          propsForDots: {
+            r: '6',
+            strokeWidth: '2',
+            stroke: '#ffa726'
+          }
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
+    );
+  };
+
   render() {
-    return <View></View>;
+    return (
+      <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+        {this.props.forecastWeather ? (
+          this.renderChart()
+        ) : (
+          <Text>Loading...</Text>
+        )}
+      </View>
+    );
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+  forecastWeather: state.weather.forecastWeather
+});
 
 const mapDispatchToProps = {
   getForecastWeatherByCity
